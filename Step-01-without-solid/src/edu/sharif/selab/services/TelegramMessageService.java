6fd6a8dfdce1ca778aaf168fdc4683608079457c
mohammad <mohammad.mo@smartech.ir>
2024-11-02 20:48:39 +0330

    feat: add telegram messaging support

---
+++ Step-01-without-solid/src/edu/sharif/selab/services/TelegramMessageService.java
@@ -0,0 +1,45 @@
+package edu.sharif.selab.services;
+
+import edu.sharif.selab.models.EmailMessage;
+import edu.sharif.selab.models.SmsMessage;
+import edu.sharif.selab.models.TelegramMessage;
+
+import java.util.regex.Pattern;
+
+public class TelegramMessageService implements MessageService {
+
+    @Override
+    public void sendSmsMessage(SmsMessage smsMessage) {
+        // Empty Body
+    }
+
+    @Override
+    public void sendEmailMessage(EmailMessage emailMessage) {
+        // Empty Body
+    }
+
+    @Override
+    public void sendTelegramMessage(TelegramMessage telegramMessage) {
+        if (validateTelegramId(telegramMessage.getSourceId()) && validateTelegramId(telegramMessage.getDestinationId())) {
+            System.out.println("Sending a Telegram Message from " + telegramMessage.getSourceId() + " to " + telegramMessage.getDestinationId() + " with content : " + telegramMessage.getContent());
+        } else {
+            throw new IllegalArgumentException("Telegram Id is Not Correct!");
+        }
+    }
+
+    private boolean validateTelegramId(String telegramId) {
+        // Check if the id is empty
+        if (telegramId == null || telegramId.isEmpty()) {
+            return false;
+        }
+
+        // Regular expression pattern for validating telegram id
+        String telegramIdRegex = "^[a-zA-Z]+\\w*[a-zA-Z0-9]+$";
+
+        // Compile the pattern into a regex Pattern object
+        Pattern pattern = Pattern.compile(telegramIdRegex);
+
+        // Check if the telegram id matches the regex pattern
+        return pattern.matcher(telegramId).matches();
+    }
+}
